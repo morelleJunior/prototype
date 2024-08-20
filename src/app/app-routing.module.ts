dd1ae85c02b3beadf27de5e2478bb7d8015e7c22
Morelle <morellejunioz@gmail.com>
2024-08-20 15:37:03 -0300

    Update appComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PhotoCaptureComponent } from './photo-capture/photo-capture.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/photo-capture', pathMatch: 'full' },
+  { path: 'photo-capture', component: PhotoCaptureComponent },
+  // Outras rotas podem ser adicionadas aqui
+  { path: '**', redirectTo: '/photo-capture' } // Redireciona para 'photo-capture' caso a rota não seja encontrada
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
